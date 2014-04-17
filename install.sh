bcf313749d939fcc70cc2a8350fd4f40ab66f320
Christof Sirk <christof.sirk@gmail.com>
2014-04-17 19:18:18 +0200

    Add global gitignore file.

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/zsh
-ln -s $(pwd -P)/$(dirname "$0")/_vim ~/.vim
+
+function create-alias() {
+if [ ! -f "$2" ]; then
+  ln -s $(pwd -P)/$(dirname "$0")/$1 $2
+fi
+}
+
+$(create-alias _vim ~/.vim)
 git submodule init
 git submodule update
 
-ln -s $(pwd -P)/$(dirname "$0")/_tmux.conf ~/.tmux.conf
-ln -s $(pwd -P)/$(dirname "$0")/gitconfig ~/.gitconfig
+$(create-alias _tmux.conf ~/.tmux.conf)
+$(create-alias gitconfig ~/.gitconfig)
+$(create-alias gitignore ~/.gitignore)
