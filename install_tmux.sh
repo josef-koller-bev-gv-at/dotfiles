e9524ecec1995d04aef41851b3f3982bae040171
Christof Sirk <christof.sirk@gmail.com>
2016-02-25 21:55:52 +0100

    Update to ruby-install 0.6.0.

--- install_tmux.sh
+++ install_tmux.sh
@@ -16,9 +16,9 @@ then
 else
   cd $USER_SOURCES
 
-  wget -O ruby-install-0.5.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.5.0.tar.gz
-  tar -xzvf ruby-install-0.5.0.tar.gz
-  cd ruby-install-0.5.0/
+  wget -O ruby-install-0.6.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.6.0.tar.gz
+  tar -xzvf ruby-install-0.6.0.tar.gz
+  cd ruby-install-0.6.0/
   sudo make install
 
   sudo -E ruby-install -i /usr/local/ ruby 2.2.1
