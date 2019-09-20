7379f0df778e3a7bca45812f554d3553f5a44713
hannamagdalena <hanna_schweizer@gmx.at>
2019-09-20 15:07:06 +0200

    Correct not known path

--- install_vcpkg.sh
+++ install_vcpkg.sh
@@ -5,7 +5,6 @@ cd vcpkg
 ./bootstrap-vcpkg.sh
 ./vcpkg integrate install
 
-echo "export VCPKG_ROOT=$USER_DOCUMENTS/vcpkg" >> /home/$USER/.zshrc.user
-echo "export VCPKG_INSTALLED_DIR=${VCPKG_ROOT}/installed/x64-linux" >> /home/$USER/.zshrc.user
-echo "export PATH=$PATH:$USER_DOCUMENTS/vcpkg" >> /home/$USER/.zshrc.user
-
+echo "export VCPKG_ROOT=/home/$USER/Documents/vcpkg" >> /home/$USER/.zshrc.user
+echo "export VCPKG_INSTALLED_DIR=/home/$USER/Documents/vcpkg/installed/x64-linux" >> /home/$USER/.zshrc.user
+echo "export PATH=$PATH:/home/$USER/Documents/vcpkg" >> /home/$USER/.zshrc.user
