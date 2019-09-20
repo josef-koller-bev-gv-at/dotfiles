fccf12c5946c0e6f81c1b03364ca11f1603a12cf
hannamagdalena <hanna_schweizer@gmx.at>
2019-09-20 12:18:52 +0200

    Add ENV variable used creating thrift files

--- install_vcpkg.sh
+++ install_vcpkg.sh
@@ -6,5 +6,6 @@ cd vcpkg
 ./vcpkg integrate install
 
 echo "export VCPKG_ROOT=$USER_DOCUMENTS/vcpkg" >> /home/$USER/.zshrc.user
+echo "export VCPKG_INSTALLED_DIR=${VCPKG_ROOT}/installed/x64-linux" >> /home/$USER/.zshrc.user
 echo "export PATH=$PATH:$USER_DOCUMENTS/vcpkg" >> /home/$USER/.zshrc.user
 
