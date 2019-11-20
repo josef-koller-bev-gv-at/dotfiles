53a62031e53970fd9c42981b08768a70f630af96
pueri <pyro@outlook.at>
2019-11-20 08:45:06 +0100

    Improve install vcpkg install script

--- install_vcpkg.sh
+++ install_vcpkg.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-git clone https://github.com/Microsoft/vcpkg.git
-cd vcpkg
+readonly current_dir=$(dirname $(readlink -f $0))
+readonly sources_dir=$(readlink -f $current_dir/..)
+readonly vcpkg_dir=$sources_dir/vcpkg
+readonly zshrc_path=$(readlink -f ~/.zshrc.user)
+
+git clone https://github.com/Microsoft/vcpkg.git $vcpkg_dir
+
+cd $vcpkg_dir
 ./bootstrap-vcpkg.sh
 ./vcpkg integrate install
 
-echo "export VCPKG_ROOT=/home/$USER/Documents/sources/vcpkg" >> /home/$USER/.zshrc.user
-echo "export VCPKG_INSTALLED_DIR=/home/$USER/Documents/sources/vcpkg/installed/x64-linux" >> /home/$USER/.zshrc.user
-echo "export PATH=$PATH:/home/$USER/Documents/sources/vcpkg" >> /home/$USER/.zshrc.user
+echo "export VCPKG_ROOT=$vcpkg_dir" >> $zshrc_path
+echo "export VCPKG_INSTALLED_DIR=$vcpkg_dir/installed/x64-linux" >> $zshrc_path
+echo "export PATH=$PATH:$vcpkg_dir" >> $zshrc_path
