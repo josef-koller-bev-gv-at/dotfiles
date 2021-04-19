3d1594dda6e5a7fd3e631f4b2a4e29cc1cd366d1
Christof Sirk <christof.sirk@gmail.com>
2021-04-19 13:39:51 +0200

    Update tmux installation

--- install_tmux.sh
+++ install_tmux.sh
@@ -8,7 +8,7 @@ USER_HOME=/home/$USER_NAME
 USER_SOURCES=$USER_HOME/Documents/sources
 INSTALLED=$USER_HOME/.installed
 
-readonly TMUX_VERSION="3.1b"
+readonly TMUX_VERSION="3.2"
 
 cd $USER_HOME
 
@@ -16,7 +16,7 @@ if [ -e $INSTALLED/tmux-$TMUX_VERSION ];
 then
 	echo "TMUX already installed."
 else
-  sudo apt-get install libevent-dev libncurses5-dev xclip automake build-essential pkg-config ruby -y
+  sudo apt-get install libevent-dev libncurses-dev xclip build-essential bison pkg-config ruby -y
   cd $USER_SOURCES
 
   wget -O tmux-$TMUX_VERSION.tar.gz https://github.com/tmux/tmux/releases/download/$TMUX_VERSION/tmux-$TMUX_VERSION.tar.gz
@@ -28,9 +28,9 @@ else
 
   sudo make install
 
-  gem install --user-install tmuxinator -v 1.1.5
-  echo "Ensure that gem bin ('~/.gem/ruby/2.5.0/bin') is in the path"
-  echo "PATH=\"\${PATH}:${HOME}/.gem/ruby/2.5.0/bin\""
+  gem install --user-install tmuxinator
+  echo "Ensure that gem bin ('~/.gem/ruby/2.7.0/bin') is in the path"
+  echo "PATH=\"\${PATH}:${HOME}/.gem/ruby/2.7.0/bin\""
 
   echo "installed TMUX successfully."
   touch $INSTALLED/tmux-$TMUX_VERSION
